632bff44d84db82ad7916d2a40adb6828209246b
Hamza HENI <hamzahenipro61@gmail.com>
2025-11-28 18:24:44 +0100

    28/11/25 2

--- index.js
+++ index.js
@@ -29,11 +29,26 @@ function eyeMovement(x,y){
         })
     });
 }
+function changeColor(eye){
+    eye.style.setProperty("--eye-color", "rgba(255, 108, 108, 1)");
+    eye.style.setProperty("--transition-length", "0.1s");
+    setTimeout(() => {
+        eye.style.setProperty("--eye-color", "whitesmoke");
+        eye.style.setProperty("--transition-length", "0.0s");
+    },3000)
+}
 function closeEyes(){
     let listOfCreatures = document.querySelectorAll(".creature");
     listOfCreatures.forEach(creature => {
         let eyes = creature.querySelectorAll(".eyeSocket");
         eyes.forEach(eyeSocket =>{
+            let changeC = () => {};
+            eyeSocket.addEventListener("click", () => {
+                    changeC = changeColor;
+                setTimeout(() => {
+                    changeC = () => {};
+                },3000)
+            });
             eyeSocket.addEventListener("mouseenter", () => {
                 let eye = eyeSocket.querySelector(".eye");
                 eye.classList.remove("openEye");
@@ -44,6 +59,7 @@ function closeEyes(){
                 let eye = eyeSocket.querySelector(".eye");
                 eye.classList.remove("closedEye");
                 eye.classList.add("openEye");
+                changeC(eye);
             });
         })
             creature.addEventListener("mouseenter", () => {
@@ -56,6 +72,27 @@ function closeEyes(){
                 let mouth = creature.querySelector(".mouth");
                 mouth.classList.remove("closedMouth");
                 mouth.classList.add("openMouth");
+            });
+            let mouth = creature.querySelector(".mouth");
+            let mouthLocation = "35%";
+            mouth.addEventListener("mouseenter", () => {
+                console.log(mouthLocation);
+                if(mouthLocation == "35%"){
+                    let value = Math.random() < 0.5 ? "0%" : "70%";
+                    mouth.style.setProperty("--left-pos",value);
+                    mouthLocation = value;
+                    console.log("at center");
+                }else{
+                    mouth.style.setProperty("--left-pos","35%");
+                    mouthLocation = "35%";
+                    console.log("not at center");
+                }
+            });
+            let mouthPlace = creature.querySelector(".mouthPlace");
+            mouthPlace.addEventListener("mouseleave", () => {
+                mouth.style.setProperty("--left-pos","35%");
+                mouthLocation = "35%";
+                console.log("left mouth place");
             });
     });
 }
